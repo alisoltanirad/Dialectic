722cdba493b022bd485c1c01311efbd9746d3cf1
alisoltanirad <soltaniradali@gmail.com>
2020-11-28 14:35:18 +0330

    Add sentence interfaces

--- logic.py
+++ logic.py
@@ -1,5 +1,33 @@
 # https://github.com/alisoltanirad/Logic.git
 
+class Sentence:
+    pass
+
+
+class Atomic(Sentence):
+    pass
+
+
+class Inverse(Sentence):
+    pass
+
+
+class Conjunction(Sentence):
+    pass
+
+
+class Disjunction(Sentence):
+    pass
+
+
+class Implication(Sentence):
+    pass
+
+
+class Iff(Sentence):
+    pass
+
+
 def main():
     pass
 
