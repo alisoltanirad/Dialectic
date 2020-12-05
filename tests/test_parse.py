5c640954e0194029e74450307d4610a1081d7a17
alisoltanirad <soltaniradali@gmail.com>
2020-12-05 22:49:51 +0330

    Update test library

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,9 +1,15 @@
-import pytest
+import unittest
 
 from dialectic import (
     Atomic, Invert, Conjunction, Disjunction, Implication, Equality
 )
 
 
-def test_atomic_parse():
-    assert Atomic('a').parse() == {'a'}
+
+class ParseTest(unittest.TestCase):
+    a = Atomic('a')
+    b = Atomic('b')
+    c = Atomic('c')
+
+    def test_atomic_parse(self):
+        assert self.a.parse() == {'a'}
