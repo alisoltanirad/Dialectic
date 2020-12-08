cbfca3ca0e89dd72369e84ebe02b958e01d8fb9b
alisoltanirad <soltaniradali@gmail.com>
2020-12-08 22:55:00 +0330

    Update inference module

--- dialectic/__init__.py
+++ dialectic/__init__.py
@@ -2,3 +2,4 @@ from .sentence import (
     Atomic, Invert, Conjunction, Disjunction, Implication, Equality
 )
 from .parse import parse_sentences
+from .inference import Inference, is_contradictory
