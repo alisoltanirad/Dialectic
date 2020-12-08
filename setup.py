712cbba3028816b5b402030c753f24db781c2efc
alisoltanirad <soltaniradali@gmail.com>
2020-12-08 18:21:53 +0330

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dialectic",
-    version="0.1.5",
+    version="0.1.6",
     description="mathematical logic",
     long_description=long_description,
     long_description_content_type="text/markdown",
