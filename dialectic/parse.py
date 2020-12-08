7ad2bf06d40b72a318b1710c3375a13fba5f02b7
alisoltanirad <soltaniradali@gmail.com>
2020-12-08 18:18:49 +0330

    Debug parse function

--- dialectic/parse.py
+++ dialectic/parse.py
@@ -2,36 +2,36 @@ from .sentence import *
 
 
 def parse(sentence_list):
-    input_set = sentence_list.copy()
-    parsed_set = set()
-    while input_set:
-        sentence = input_set.pop()
+    input_list = sentence_list.copy()
+    parsed_list = []
+    while input_list:
+        sentence = input_list.pop()
 
         if (type(sentence) is Atomic) or (type(sentence) is Invert):
-            parsed_set.add(sentence)
+            parsed_list.add(sentence)
 
         elif type(sentence) is Conjunction:
-            input_set.add(sentence.lchild)
-            input_set.add(sentence.rchild)
+            input_list.add(sentence.lchild)
+            input_list.add(sentence.rchild)
 
         elif type(sentence) is Disjunction:
-            if (~sentence.lchild).validate(input_set | parsed_set):
-                input_set.add(sentence.rchild)
-            elif (~sentence.rchild).validate(input_set | parsed_set):
-                input_set.add(sentence.lchild)
+            if (~sentence.lchild).validate(input_list + parsed_list):
+                input_list.add(sentence.rchild)
+            elif (~sentence.rchild).validate(input_list + parsed_list):
+                input_list.add(sentence.lchild)
 
         elif type(sentence) is Implication:
-            if sentence.lchild.validate(input_set | parsed_set):
-                input_set.add(sentence.rchild)
+            if sentence.lchild.validate(input_list + parsed_list):
+                input_list.add(sentence.rchild)
 
         elif type(sentence) is Equality:
-            if sentence.lchild.validate(input_set | parsed_set):
-                input_set.add(sentence.rchild)
-            elif sentence.rchild.validate(input_set | parsed_set):
-                input_set.add(sentence.lchild)
-            elif (~sentence.lchild).validate(input_set | parsed_set):
-                input_set.add(~sentence.rchild)
-            elif (~sentence.rchild).validate(input_set | parsed_set):
-                input_set.add(~sentence.lchild)
+            if sentence.lchild.validate(input_list + parsed_list):
+                input_list.add(sentence.rchild)
+            elif sentence.rchild.validate(input_list + parsed_list):
+                input_list.add(sentence.lchild)
+            elif (~sentence.lchild).validate(input_list + parsed_list):
+                input_list.add(~sentence.rchild)
+            elif (~sentence.rchild).validate(input_list + parsed_list):
+                input_list.add(~sentence.lchild)
 
-    return parsed_set
+    return set(parsed_list)
