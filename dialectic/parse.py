f13bf6a0cd69eb2c0d4aa644a44507667c5f7ecb
alisoltanirad <soltaniradali@gmail.com>
2020-12-08 18:21:32 +0330

    Debug parse function

--- dialectic/parse.py
+++ dialectic/parse.py
@@ -8,30 +8,30 @@ def parse(sentence_list):
         sentence = input_list.pop()
 
         if (type(sentence) is Atomic) or (type(sentence) is Invert):
-            parsed_list.add(sentence)
+            parsed_list.append(sentence)
 
         elif type(sentence) is Conjunction:
-            input_list.add(sentence.lchild)
-            input_list.add(sentence.rchild)
+            input_list.append(sentence.lchild)
+            input_list.append(sentence.rchild)
 
         elif type(sentence) is Disjunction:
             if (~sentence.lchild).validate(input_list + parsed_list):
-                input_list.add(sentence.rchild)
+                input_list.append(sentence.rchild)
             elif (~sentence.rchild).validate(input_list + parsed_list):
-                input_list.add(sentence.lchild)
+                input_list.append(sentence.lchild)
 
         elif type(sentence) is Implication:
             if sentence.lchild.validate(input_list + parsed_list):
-                input_list.add(sentence.rchild)
+                input_list.append(sentence.rchild)
 
         elif type(sentence) is Equality:
             if sentence.lchild.validate(input_list + parsed_list):
-                input_list.add(sentence.rchild)
+                input_list.append(sentence.rchild)
             elif sentence.rchild.validate(input_list + parsed_list):
-                input_list.add(sentence.lchild)
+                input_list.append(sentence.lchild)
             elif (~sentence.lchild).validate(input_list + parsed_list):
-                input_list.add(~sentence.rchild)
+                input_list.append(~sentence.rchild)
             elif (~sentence.rchild).validate(input_list + parsed_list):
-                input_list.add(~sentence.lchild)
+                input_list.append(~sentence.lchild)
 
     return set(parsed_list)
