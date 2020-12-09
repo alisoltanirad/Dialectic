7d7c57af4a10a5ac4c2d370ba5a44bc78ebc1573
alisoltanirad <soltaniradali@gmail.com>
2020-12-09 15:40:13 +0330

    Debug

--- dialectic/sentence.py
+++ dialectic/sentence.py
@@ -57,7 +57,7 @@ class Invert(Sentence):
         return self.operator + str(self.child)
 
     def __hash__(self):
-        return hash(not self.child)
+        return hash(~self.child)
 
     def validate(self, set):
         if type(self.child) is Atomic:
