1ae627f160390c615b5e3e6c2f602c25a77fed80
alisoltanirad <soltaniradali@gmail.com>
2023-02-04 11:55:45 -0500

    Update setup file

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open("README.md", "r") as fh:
 setup(
     name="dialectic",
     version="0.3.2",
-    description="Mathematical Logic",
+    description="Mathematical Logic Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alisoltanirad/dialectic",
-    author="Ali Soltani Rad",
+    author="Ali Soltanirad",
     author_email="soltaniradali@gmail.com",
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
     ],
-    python_requires='>=3.7'
+    python_requires=">=3.11"
 )
